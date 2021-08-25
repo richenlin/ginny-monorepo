ad1cd7d7a0212d6b836968f2dac680348c146131
linyyyang <linyyyang@tencent.com>
2021-08-25 11:55:41 +0800

    🐞 fix:修复文件错误

--- config.go
+++ config.go
@@ -1,107 +1,76 @@
-package config
+package log
 
 import (
-	"bytes"
-	"io/ioutil"
-	"log"
 	"os"
-	"strings"
 
 	"github.com/google/wire"
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
-const (
-	defaultConfigPath = "."
-)
+// Options is log configuration struct
+type Options struct {
+	Filename   string
+	MaxSize    int
+	MaxBackups int
+	MaxAge     int
+	Level      string
+	Stdout     bool
+}
 
-// Init 初始化viper
-func New(path string) (*viper.Viper, error) {
+func NewOptions(v *viper.Viper) (*Options, error) {
 	var (
 		err error
-		v   = viper.New()
+		o   = new(Options)
 	)
-
-	v.AddConfigPath(defaultConfigPath)
-	v.SetConfigFile(string(path))
-
-	v.AutomaticEnv()
-	v.SetEnvPrefix("ginny")
-	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-
-	// if err := v.ReadInConfig(); err == nil {
-	// 	log.Printf("Config %s loaded successfully...", v.ConfigFileUsed())
-	// } else {
-	// 	return nil, err
-	// }
-	if err := loadConfig(v); err != nil {
+	if err = v.UnmarshalKey("log", o); err != nil {
 		return nil, err
 	}
 
-	return v, err
+	return o, err
 }
 
-// loadConfig
-func loadConfig(v *viper.Viper) error {
-	log.Println("Loading config...")
-	data, err := ioutil.ReadFile(v.ConfigFileUsed())
+// New for init zap log library
+func New(o *Options) (*zap.Logger, error) {
+	var (
+		err    error
+		level  = zap.NewAtomicLevel()
+		logger *zap.Logger
+	)
+
+	err = level.UnmarshalText([]byte(o.Level))
 	if err != nil {
-		return err
+		return nil, err
 	}
-	log.Println("Getting environment variables...")
-	conf := expandEnv(string(data))
-	err = v.ReadConfig(bytes.NewReader([]byte(conf)))
-	if err != nil {
-		return err
+
+	fw := zapcore.AddSync(&lumberjack.Logger{
+		Filename:   o.Filename,
+		MaxSize:    o.MaxSize, // megabytes
+		MaxBackups: o.MaxBackups,
+		MaxAge:     o.MaxAge, // days
+	})
+
+	cw := zapcore.Lock(os.Stdout)
+
+	// file core 采用jsonEncoder
+	cores := make([]zapcore.Core, 0, 2)
+	je := zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+	cores = append(cores, zapcore.NewCore(je, fw, level))
+
+	// stdout core 采用 ConsoleEncoder
+	if o.Stdout {
+		ce := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+		cores = append(cores, zapcore.NewCore(ce, cw, level))
 	}
-	return nil
+
+	core := zapcore.NewTee(cores...)
+	logger = zap.New(core)
+
+	zap.ReplaceGlobals(logger)
+
+	return logger, err
 }
 
-// expandEnv 寻找s中的 ${var} 并替换为环境变量的值，没有则替换为空，不解析 $var
-func expandEnv(s string) string {
-	var buf []byte
-	i := 0
-	for j := 0; j < len(s); j++ {
-		if s[j] == '$' && j+2 < len(s) && s[j+1] == '{' { // 只匹配${var} 不匹配$var
-			if buf == nil {
-				buf = make([]byte, 0, 2*len(s))
-			}
-			buf = append(buf, s[i:j]...)
-			name, w := getShellName(s[j+1:])
-			if name == "" && w > 0 {
-				// 非法匹配，去掉$
-			} else if name == "" {
-				buf = append(buf, s[j]) // 保留$
-			} else {
-				buf = append(buf, os.Getenv(name)...)
-			}
-			j += w
-			i = j + 1
-		}
-	}
-	if buf == nil {
-		return s
-	}
-	return string(buf) + s[i:]
-}
-
-// getShellName 获取占位符的key，即${var}里面的var内容
-// 返回 key内容 和 key长度
-func getShellName(s string) (string, int) {
-	// 匹配右括号 }
-	// 输入已经保证第一个字符是{，并且至少两个字符以上
-	for i := 1; i < len(s); i++ {
-		if s[i] == ' ' || s[i] == '\n' || s[i] == '"' { // "xx${xxx"
-			return "", 0 // 遇到上面这些字符认为没有匹配中，保留$
-		}
-		if s[i] == '}' {
-			if i == 1 { // ${}
-				return "", 2 // 去掉${}
-			}
-			return s[1:i], i + 1
-		}
-	}
-	return "", 0 // 没有右括号，保留$
-}
-
-var ProviderSet = wire.NewSet(New)
+var ProviderSet = wire.NewSet(New, NewOptions)
--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.16
 require (
 	github.com/google/wire v0.5.0
 	github.com/spf13/viper v1.8.1
+	go.uber.org/zap v1.17.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 )
